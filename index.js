42a2a46a7b92f79ec3c16778478839a160a607d5
florence ajadi <florence.ajadi@yahoo.com>
2022-05-16 14:55:45 -0500

    commit tree 2c8c5b8145e94a1953798d6de5f3374d455386c9

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(weekend="roller-skate") {
+    return(`This Saturday, I want to ${weekend}!`);
+}
+saturdayFun()
+
+function mondayWork(weekday="go to the office") {
+    return(`This Monday, I will ${weekday}.`);
+}
+
+mondayWork()
+
+
+function wrapAdjective(style="*") {
+    return function (string="special"){
+     return (`You are ${style}${string}${style}!`);   
+    }
+}
